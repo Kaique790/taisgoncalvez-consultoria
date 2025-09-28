90b40723a727a5bd847b29fa08df57f2b7cdb48e
Kaique790 <kiaqbs@gmail.com>
2025-09-28 16:59:09 -0300

    feat: add home section

---
+++ assets/js/scripts.js
@@ -0,0 +1,7 @@
+const navbar = document.getElementById("navbar");
+const navbarBtn = document.getElementById("navbar-btn");
+
+function toggleNavbar() {
+  navbar.classList.toggle("active");
+  navbarBtn.classList.toggle("active");
+}
