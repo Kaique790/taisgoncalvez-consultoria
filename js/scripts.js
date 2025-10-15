d285d6e985b2437d540462ab07b053325fbacc59
Kaique790 <kiaqbs@gmail.com>
2025-10-15 15:39:51 -0300

    feat: add hover animations in outline btn

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
 const mobileNavbar = document.getElementById("mobile-navbar");
+const mobileOverlay = document.getElementById("mobile-overlay");
 
 function toggleNavbar() {
   mobileNavbar.classList.toggle("active");
+  mobileOverlay.classList.toggle("active");
 }
